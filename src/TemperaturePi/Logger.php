49acfe5b7575f6092962d4dac4c0639a14ce43f8
Ronan Guilloux <ronan.guilloux@gmail.com>
2012-11-27 00:34:26 +0100

    executable, bash-compatible, thermometer php file + revamped KISS-based Logger class

--- src/TemperaturePi/Logger.php
+++ src/TemperaturePi/Logger.php
@@ -2,16 +2,39 @@
 
 namespace TemperaturePi;
 
-use PhpGpio\Sensors\DS18B20();
+use PhpGpio\Sensors\DS18B20;
 
-class Logger
+class Logger extends \SQlite3
 {
     private $sensor;
     private $currentTemperature;
+    private $dbPath;
+    private $db;
 
     public function __construct()
     {
         $this->sensor = new DS18B20();
-        $this->currentTemperature = sensor->read();
+        $this->dbPath = __DIR__ . '/../../resources/sqlite/log.db';
+        $this->db = 'temperature';
     }
+
+    public function persist($reset = false)
+    {
+        $this->currentTemperature = $this->sensor->read();
+        $this->open($this->dbPath);
+        if($reset) {
+            $this->exec('DROP TABLE temperature');
+        }
+        $this->exec('CREATE TABLE IF NOT EXISTS temperature (datetime DATETIME, celsius FLOAT)');
+        $this->exec("INSERT INTO temperature (datetime, celsius) VALUES (datetime('NOW'), " . $this->currentTemperature . ")");
+        /*
+        $result = $this->query('SELECT datetime, celsius FROM temperature');
+        while ($row = $result->fetchArray()) {
+            var_dump($row);
+        }
+        */
+        echo "\n" . date("d/m/Y H:i:s") . " : " . $this->currentTemperature;
+        $this->close();
+    }
+
 }
